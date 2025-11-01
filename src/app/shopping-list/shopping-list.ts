271d7c847f17a30085b4d5cf898b4bd65c01edfe
NIPUNMADHUSANKA <nipunmadhusanka201080@gmail.com>
2025-11-01 11:18:13 +0530

    Add shopping Item Form

--- src/app/shopping-list/shopping-list.ts
+++ src/app/shopping-list/shopping-list.ts
@@ -2,17 +2,24 @@ import { Component, signal } from '@angular/core';
 import { ShoppingItem } from '../shopping-item/shopping-item';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MatIconModule } from '@angular/material/icon';
-import { NgClass } from '@angular/common';
+import { CommonModule, NgClass } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
+import { ShoppingListItemForm } from '../shopping-list-item-form/shopping-list-item-form';
 
 @Component({
   selector: 'app-shopping-list',
-  imports: [ShoppingItem, MatExpansionModule, MatIconModule, NgClass, MatButtonModule],
+  imports: [ShoppingItem, MatExpansionModule, MatIconModule, NgClass, MatButtonModule, ShoppingListItemForm, CommonModule],
   templateUrl: './shopping-list.html',
   styleUrl: './shopping-list.scss',
 })
 export class ShoppingList {
   readonly panelOpenState = signal(true);
   
+    protected createShoppingItem = signal<boolean>(false);
+
+    openCreateItemDialog(){
+      this.createShoppingItem.set(!this.createShoppingItem());
+    }
+ 
 
 }
